be87dcf23dd3ee8873b9a6cb5b090443c0c2d577
sumanth1231807 <sumanthravi11111@gmail.com>
2022-04-04 15:54:56 +0530

    m

--- EmployeeFlask.py
+++ EmployeeFlask.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template
 
 app=Flask(__name__)
-
+@app.route('/')
+def hello():
+    return "Hello"
 @app.route('/Empdetails')
 def welcome():
     return render_template("Empdetails.html")
-@app.route('/search')
-def search_page():
-    return render_template("search.html")
-@app.route('/update')
-def update_page():
-    return render_template("update.html")
-@app.route('/delete')
-def delete_page():
-    return render_template("delete.html")
+# @app.route('/search')
+# def search_page():
+#     return render_template("search.html")
+# @app.route('/update')
+# def update_page():
+#     return render_template("update.html")
+# @app.route('/delete')
+# def delete_page():
+#     return render_template("delete.html")
 
 
 
